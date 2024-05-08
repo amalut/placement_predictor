a88c087cd0fff2075de2f2d418dcc5a092464907
amalut <amalut9@gmail.com>
2024-05-08 12:39:33 +0530

    Updated

--- model.py
+++ model.py
@@ -34,9 +34,9 @@ model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 
 # Evaluate the model
-print("Accuracy:", accuracy_score(y_test, predictions))
-print("\nConfusion Matrix:\n", confusion_matrix(y_test, predictions))
-print("\nClassification Report:\n", classification_report(y_test, predictions))
+#print("Accuracy:", accuracy_score(y_test, predictions))
+#print("\nConfusion Matrix:\n", confusion_matrix(y_test, predictions))
+#print("\nClassification Report:\n", classification_report(y_test, predictions))
 
 # Save the trained model to a file
 pickle.dump(model, open('model.pkl', 'wb'))
@@ -50,17 +50,17 @@ average_values = {
 }
 
 # Predict the placement of a new student
-new_student_stream = 'Information Technology'  # Replace with the correct stream
-new_student_stream_dummy = stream_dummies.loc[stream_dummies[f'Stream_{new_student_stream}'] == 1].values.tolist()[0]
-new_student_features = [7.5, 7, 5, 2] + new_student_stream_dummy  # Add the stream dummy variables
+#new_student_stream = 'Information Technology'  # Replace with the correct stream
+#new_student_stream_dummy = stream_dummies.loc[stream_dummies[f'Stream_{new_student_stream}'] == 1].values.tolist()[0]
+#new_student_features = [7.5, 7, 5, 2] + new_student_stream_dummy  # Add the stream dummy variables
 
-prediction = model.predict([new_student_features])
-probability_new_student = model.predict_proba([new_student_features])[0][1]
+#prediction = model.predict([new_student_features])
+#probability_new_student = model.predict_proba([new_student_features])[0][1]
 
 # Call the recommendation function
-recommendations = recommend_for_student(probability_new_student,new_student_features, average_values)
-print("Predicted placement:", prediction[0])
-print("Predicted probability of getting placed:", probability_new_student * 100, "%")
-print(type(recommendations))
-for recommendation in recommendations:
-    print(recommendation)
+#recommendations = recommend_for_student(probability_new_student,new_student_features, average_values)
+#print("Predicted placement:", prediction[0])
+#print("Predicted probability of getting placed:", probability_new_student * 100, "%")
+#print(type(recommendations))
+#for recommendation in recommendations:
+   # print(recommendation)
---
+++ run.py
@@ -0,0 +1,2 @@
+from PREDICTOR import create_app
+app=create_app()
